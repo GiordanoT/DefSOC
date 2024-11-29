fd56ff42798c8801a5612501a484073f56384400
GiordanoT <giordanotinella@gmail.com>
2024-11-29 12:10:42 +0100

    summary text fix

--- frontend/src/common/U.ts
+++ frontend/src/common/U.ts
@@ -9,4 +9,18 @@ export default class U {
     public static sleep(s: number): Promise<void> {
         return new Promise((resolve) => setTimeout(resolve, s * 1000));
     }
+    public static extract(fullString: string, startString: string, endString: string): string[] {
+        const results: string[] = [];
+        let startIndex = 0;
+        while (true) {
+            startIndex = fullString.indexOf(startString, startIndex);
+            if (startIndex === -1) break;
+            startIndex += startString.length;
+            const endIndex = fullString.indexOf(endString, startIndex);
+            if (endIndex === -1) break;
+            results.push(fullString.substring(startIndex, endIndex));
+            startIndex = endIndex + endString.length;
+        }
+        return results;
+    }
 }
--- frontend/src/components/tabs/Summary.tsx
+++ frontend/src/components/tabs/Summary.tsx
@@ -1,15 +1,23 @@
 import {Result} from '../../common/types';
+import U from '../../common/U';
 type Props = {data: Result};
 
 function Summary(props: Props) {
     const {data} = props;
+    const text = data.summary_text_en.replaceAll('- ', '');
+    const titles = U.extract(text, '**', '**');
+    const content = U.extract(text.slice(2) + '**', '**', '**');
+
     return(<div className={'p-2'}>
         <div>ID: <b>{data.idsummary}</b></div>
         <div>Creation Date: <b>{data.creation_date}</b></div>
         <div>Last Edit: <b>{data.last_edit}</b></div>
         <div>Domain Name: <b>{data.domain_name}</b></div>
         <hr />
-        {data.summary_text_en}
+        {titles.map((_, index) => <div key={index}>
+            <b>{titles[index]}</b>
+            <label>{content[index]}</label>
+        </div>)}
     </div>);
 }
 
